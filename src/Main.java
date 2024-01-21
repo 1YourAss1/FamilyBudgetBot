5953e5879441ecd5e6bf6a95ce1e2d35cca8ed1f
YourAss <yrik.murashko@gmail.com>
2024-01-21 23:27:19 +0300

    Add send message function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-       new TelegramBot().start(System.getenv("TELEGRAM_BUDGETIK_BOT_TOKEN"));
+        int[] user_id = Arrays.stream(System.getenv("TELEGRAM_BUDGETIK_BOT_USER_ID").split(",")).mapToInt(Integer::parseInt).toArray();
+        new TelegramBot(System.getenv("TELEGRAM_BUDGETIK_BOT_TOKEN"), user_id).start();
     }
 }
--- src/TelegramBot.java
+++ src/TelegramBot.java
@@ -3,15 +3,25 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
 public class TelegramBot {
     private static final String URL_API_TELEGRAM = "https://api.telegram.org/bot";
     private static final int TIMEOUT = 10;
+    private final String token;
+    private final int[] user_id_array;
 
-    public void start(String token) {
+    TelegramBot(String token, int[] user_id) {
+        this.token = token;
+        this.user_id_array = user_id;
+    }
+
+    public void start() {
         System.out.println("Start Family Budget Telegram Bot");
         System.out.println("-----------------------------------");
         // Check token
@@ -29,12 +39,14 @@ public class TelegramBot {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JSONObject jsonResponse = new JSONObject(response.body());
             if ((Boolean) jsonResponse.get("ok")) {
-                System.out.println("Bot is working");
+                System.out.println("Bot is active");
             } else {
+                System.err.println("Bot is NOT active");
                 System.exit(0);
             }
         } catch (IOException | InterruptedException e) {
             System.err.println("ERROR: " + e.getMessage());
+            System.exit(0);
         }
         System.out.println("-----------------------------------");
         // Start
@@ -57,11 +69,37 @@ public class TelegramBot {
                     offset = (int) ((JSONObject) result.get(0)).get("update_id") + 1;
                     // Check user id
                     JSONObject message = (JSONObject) ((JSONObject) result.get(0)).get("message");
-                    System.out.println(message.get("text"));
+                    JSONObject from = (JSONObject) message.get("from");
+                    int from_id = from.getInt("id");
+                    if (IntStream.of(user_id_array).anyMatch(user_id -> user_id == from_id)) {
+                        System.out.println(from_id + ": " + message.get("text"));
+                    } else {
+                        sendMessage(from_id, "Доступ запрещен");
+                    }
                 }
             } catch (IOException | InterruptedException e) {
                 System.err.println("ERROR: " + e.getMessage());
             }
+        }
+    }
+
+    private void sendMessage(int chat_id, String text) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL_API_TELEGRAM + token + "/sendMessage"
+                        + "?chat_id=" + chat_id
+                        + "&text=" + URLEncoder.encode(text, StandardCharsets.UTF_8)))
+                .GET()
+                .build();
+        try (HttpClient client = HttpClient.newHttpClient()){
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject jsonResponse = new JSONObject(response.body());
+            if (!(Boolean) jsonResponse.get("ok")) {
+                System.err.println("ERROR: Cant send message: error_code "
+                        + jsonResponse.get("error_code") + " - " + jsonResponse.get("description"));
+            }
+        } catch (IOException | InterruptedException e) {
+            System.err.println("ERROR: " + e.getMessage());
+            System.exit(0);
         }
     }
 }
